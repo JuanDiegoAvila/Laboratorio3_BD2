aae9bb31860904e0b31bd2e76686d9d402f87163
JuanDiegoAvila <juanavila20027@gmail.com>
2023-02-02 10:13:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pymongo
 import csv
-import json
-import pandas as pd
 
 client = pymongo.MongoClient('mongodb+srv://avilin:pilin@mondongo.lnml9zm.mongodb.net/?retryWrites=true&w=majority')
 db = client['Mondongo']
@@ -10,8 +8,6 @@ collection = db['winemag-data']
 with open("winemag-data.csv", "r", encoding='utf-8') as file:
     reader = csv.DictReader(file)
     data = list(reader)
-
-
 
 for i in range(0, len(data), 20000):
     chunk = data[i:i+20000]
